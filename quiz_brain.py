97662412cde292dcfca0c638810334b8acaf7091
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-07-20 20:01:02 +0700

    Add score

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,6 +1,7 @@
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
 
     def next_q(self):
@@ -10,9 +11,10 @@ class QuizBrain:
         text = self.question_list[self.question_number].text
         ans = self.question_list[self.question_number].answer
         if input(text+"(True/False)").title() == ans:
-            print("You got it!")
+            self.score += 1
+            print(f"You got it!\nScore: {self.score}")
             return False
         else:
-            print("Nope...")
+            print(f"Nope...\nScore: {self.score}")
             return True
 
