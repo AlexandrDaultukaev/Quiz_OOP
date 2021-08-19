4637e1775bec80d3a7b630b17ecd474b344e9035
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-08-19 12:53:25 +0700

    Add buttons

--- ui.py
+++ ui.py
@@ -9,16 +9,24 @@ class QuizUI:
         self.window.minsize(500, 600)
         self.bg_canvas = Canvas(width=500, height=600, bg="#375362", highlightthickness=0)
         true_img = PhotoImage(file="./images/true.png")
-        self.bg_canvas.create_image(350, 470, image=true_img)
         false_img = PhotoImage(file="./images/false.png")
-        self.bg_canvas.create_image(150, 470, image=false_img)
+        self.button_true = Button(image=true_img, borderwidth=0, highlightthickness=0, state="normal", command=lambda:
+        self.true_click())
+        self.button_false = Button(image=false_img, borderwidth=0, highlightthickness=0, state="normal", command=lambda:
+        self.false_click())
+        self.button_true.place(x=300, y=470)
+        self.button_false.place(x=100, y=470)
         self.bg_canvas.pack()
         self.window.config(bg="black")
         self.canvas = Canvas(width=350, height=350, bg="white", highlightthickness=0)
         self.canvas.place(x=75, y=45)
         self.score = self.bg_canvas.create_text(125, 20, text="score: 0", font=("Arial", 20, "normal"), fill="white")
-
         self.window.mainloop()
 
+    def true_click(self):
+        print("T")
+
+    def false_click(self):
+        print("F")
 
 q = QuizUI()
